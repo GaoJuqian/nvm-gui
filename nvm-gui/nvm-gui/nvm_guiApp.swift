f069d016750dfb36ca7c5791c51570c8593a4877
cyRenShui <jqiown@163.com>
2023-03-14 23:53:51 +0800

    init

---
+++ nvm-gui/nvm-gui/nvm_guiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nvm_guiApp.swift
+//  nvm-gui
+//
+//  Created by shui ren on 2023/3/14.
+//
+
+import SwiftUI
+
+@main
+struct nvm_guiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
