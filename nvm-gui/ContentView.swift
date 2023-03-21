61248474184f9b13cd8f5f3f551fd5d2e0631cbd
cyRenShui <jqiown@163.com>
2023-03-21 19:31:54 +0800

    🤯

--- nvm-gui/ContentView.swift
+++ nvm-gui/ContentView.swift
@@ -6,20 +6,57 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    @State private var out:String?
+    
+    func shell(_ args: String...) -> Int32 {
+      let task = Process()
+      task.launchPath = "/bin/zsh"
+      task.arguments = args
+      task.launch()
+      task.waitUntilExit()
+      return task.terminationStatus
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(out ?? "null")
+            .padding()
+            .onAppear {
+                print(shell("-l", "-c", "echo $HOME; nvm list"))
+                // /Users/gjq/Library/Containers/com.gaojuqian.nvm-gui/Data
+                // zsh:1: command not found: nvm
+            }
     }
 }
 
+
+
+
+
+
+
+
+//struct ContentView: View {
+//    let test = 213
+//    @State private var abc:String?
+//
+//
+//    var body: some View {
+//        VStack {
+//            Text(abc ?? "")
+//            Button("Button") {
+//
+//                abc = NvmControl.test()
+//            }
+//        }
+//        .padding()
+//    }
+//}
+
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         ContentView()
     }
